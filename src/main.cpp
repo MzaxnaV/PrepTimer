5062e3ac5297918af6914296ba1f4a43e1c1c097
MzaxnaV <30342951+MzaxnaV@users.noreply.github.com>
2026-02-21 00:51:14 +0530

    set up cmake build

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include "imgui.h"
+#include "imgui_impl_win32.h"
+#include "imgui_impl_dx11.h"
+#include "implot.h"
+
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+    return 0;
+}
